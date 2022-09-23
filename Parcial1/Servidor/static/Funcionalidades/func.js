2c2dafcc3cb7b0d8fe664edebd84c031e5cd44b1
HernandezLo <luishernandez0626@outlook.com>
2022-09-23 08:04:29 -0500

    Funcionalidades

--- Parcial1/Servidor/static/Funcionalidades/func.js
+++ Parcial1/Servidor/static/Funcionalidades/func.js
@@ -25,25 +25,33 @@ app.post('/texto', (req,res) => {
    })
 }) 
 
-app.post('/texto', (req,res) => {
+app.post('/json', (req,res) => {
     // res.send('Servidor Express contestando a get desde el pto 8082')
-    console.log(req.body)
-    let may = req.body.toUpperCase()
-    let sinesp = req.body.trim()
-    let longi = req.body.length
-    res.json({
-     mayusculas: may,
-     sinespacios: sinesp,
-     longitud: longi
-    })
- }) 
+    console.log(req.body.nombre)
+    let cadena = "Hola " + req.body.nombre+ " " + req.body.apellido + " como estas?"
+    res.json({saludo:cadena})
+    
+}) 
+
+app.get('/mayusculas/:cadena', (req,res) => {
+    console.log(req.params)
+    res.send(req.params)
+}) 
+
+app.get('/suma', (req,res) => {
+    console.log(req.query)
+    let suma = parseInt(req.query.x)+parseInt(req.query.y)
+    res.send(`La suma es ${suma}`)
+}) 
+
+
 
 app.use('/', (req,res) => {
     // res.send('Servidor Express contestando a get desde el pto 8082')
     res.status(404).sendFile('./static/html/404.html', {root: __dirname})
  }) 
 
-app.listen(8082, ()=>{
+app.listen(8083, ()=>{
     console.log('Servidor corriendo express desde 8082')
     console.log(__dirname)
     console.log(__filename)
