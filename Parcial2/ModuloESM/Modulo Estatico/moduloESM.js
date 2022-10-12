a33270478578bb971bf371f9adf8fd91fae4e79e
HernandezLo <luishernandez0626@outlook.com>
2022-10-12 15:48:11 -0500

    ESM

---
+++ Parcial2/ModuloESM/Modulo Estatico/moduloESM.js
@@ -0,0 +1,13 @@
+export function pasarMayusculas(cadena) {
+    return cadena.toUpperCase();
+}
+
+export function quitarEspacios(cadena) {
+    return cadena.replace(/ /g,"");
+}
+
+export function obtenerLongitud(cadena)
+{
+    return cadena.length;
+}
+
