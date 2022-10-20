53ede879e2779883e0fa17b5e8e56f47a312d430
HernandezLo <luishernandez0626@outlook.com>
2022-10-19 23:57:59 -0500

    CRUD

--- Parcial2/Servidorbd/Servidorbd.js
+++ Parcial2/Servidorbd/Servidorbd.js
@@ -1,7 +1,6 @@
 const express = require('express')
-const app = express()
 var mySQL = require('mysql'); 
-
+const app = express()
 
 var con = mySQL.createConnection({
     host     : "localhost",
@@ -10,13 +9,27 @@ var con = mySQL.createConnection({
     database : "l18100188"
 });
 
-con.connect();
+con.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySQL esta conectado')
+});
 
-app.get('/test/:id', async (req,res) => {
-    const DataId = req.params.id;
-    console.log(DataId)
-    const [responseDB] = await con.query(`SELECT * FROM registro WHERE NControl = ${DataId}`);
-    res.json(responseDB)
+app.get('/get', (req, res) => {
+    let sql = 'SELECT * FROM registro';
+    let query = con.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
 }) 
 
-app.listen(8081, ()=>{console.log('Servidor corriendo express')})
+app.get('/get/:NumControl', (req, res) => {
+    let sql = `SELECT * FROM registro WHERE NumControl = ${req.params.NumControl}`;
+    let query = con.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+}) 
+
+app.listen(8089, ()=>{console.log('Servidor corriendo express')})
