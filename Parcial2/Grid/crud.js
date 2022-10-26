f1e489452e67f5c4f602654492c0367416ff1374
HernandezLo <luishernandez0626@outlook.com>
2022-10-25 21:15:31 -0500

    grid

---
+++ Parcial2/Grid/crud.js
@@ -0,0 +1,84 @@
+const { query } = require('express');
+const express = require('express')
+var mySQL = require('mysql'); 
+const app = express()
+
+var con = mySQL.createConnection({
+    host     : "localhost",
+    user     : "root",
+    password : "123456",
+    database : "l18100188"
+});
+
+con.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySQL esta conectado')
+});
+
+//Get (todos)
+app.get('/get', (req, res) => {
+    let sql = 'SELECT * FROM registro';
+    let query = con.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    });
+}); 
+
+//Get (uno)
+app.get('/get/:NumControl', (req, res) => {
+    let sql = `SELECT * FROM registro WHERE NumControl = ${req.params.NumControl}`;
+    let query = con.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    });
+}); 
+
+//Post
+app.post('/post', (req, res) => {
+    console.log(req.query);
+
+    let params = {
+        'NumControl': req.query.NumControl,
+        'NombreEs': req.query.NombreEs,
+        'ApellidoPa': req.query.ApellidoPa,
+        'ApellidoMa': req.query.ApellidoMa
+    }
+
+    let sql = 'Insert into registro SET ?';
+    let query = con.query(sql,params,(err,result)=>{
+        if(err) throw err;
+        res.send('Insert efectuado')
+    });
+}); 
+
+//Put
+app.put('/put/:NumControl', (req, res) => {
+    console.log(req.query);
+
+    let querys = {
+        
+        'NombreEs': req.query.NombreEs,
+        'ApellidoPa': req.query.ApellidoPa,
+        'ApellidoMa': req.query.ApellidoMa
+    }
+
+    let sql = `UPDATE registro SET ? WHERE NumControl = ${req.params.NumControl}`;
+
+    let query = con.query(sql,querys,(err,result)=>{
+        if(err) throw err;
+        res.send('Update efectuado')
+    });
+}); 
+
+//Delete
+app.delete('/delete/:NumControl', (req, res) => {
+    let sql = `DELETE FROM registro WHERE NumControl = ${req.params.NumControl}`;
+    let query = con.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send(`Eliminado correctamente todos los datos del NumControl: ${req.params.NumControl}`)
+    });
+}); 
+
+app.listen(8091, ()=>{console.log('Servidor corriendo express')})
--- Parcial2/Grid/grid.js
+++ Parcial2/Grid/grid.js
@@ -1,24 +1,14 @@
-import { Grid } from "gridjs";
-import "./node_modules/gridjs/dist/theme/mermaid.css";
-
-function helloWorld () {
-  const wrapperRef = useRef(null);
-
-  const grid = new Grid({
+new gridjs.Grid({
     columns: ['NumControl', 'NombreEs', 'ApellidoPa', 'ApellidoMa'],
     server: {
-        url: 'http://localhost:8090/get',
-        then: data => data.map(ejemplo => 
+        url: 'http://localhost:8091/get',
+        then: data => data.results.map(ejemplo => 
             [ejemplo.NumControl, ejemplo.NombreEs, ejemplo.ApellidoPa, ejemplo.ApellidoMa]
         )
 
     }
-  });
+  }).render(document.getElementById("wrapper"));
   
-  useEffect(() => {
-    grid.render(wrapperRef.current);
-  });
-  
-  return <div ref={wrapperRef} />;
-}
+
+   
 
