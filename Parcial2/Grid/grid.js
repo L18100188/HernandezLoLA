a2294a1169eda5a0afe4b534ba4bb8b4b8db54b8
Luis Hdz <luishernandez0626@outlook.com>
2022-10-25 20:44:50 -0500

    grid

--- Parcial2/Grid/grid.js
+++ Parcial2/Grid/grid.js
@@ -1,1 +1,24 @@
-//descargar e instalar el modulo Grid.JS
+import { Grid } from "gridjs";
+import "./node_modules/gridjs/dist/theme/mermaid.css";
+
+function helloWorld () {
+  const wrapperRef = useRef(null);
+
+  const grid = new Grid({
+    columns: ['NumControl', 'NombreEs', 'ApellidoPa', 'ApellidoMa'],
+    server: {
+        url: 'http://localhost:8090/get',
+        then: data => data.map(ejemplo => 
+            [ejemplo.NumControl, ejemplo.NombreEs, ejemplo.ApellidoPa, ejemplo.ApellidoMa]
+        )
+
+    }
+  });
+  
+  useEffect(() => {
+    grid.render(wrapperRef.current);
+  });
+  
+  return <div ref={wrapperRef} />;
+}
+
