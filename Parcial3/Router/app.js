32bf4213084fef7d1361256890e34165313b85b7
HernandezLo <luishernandez0626@outlook.com>
2022-11-18 21:06:54 -0600

    Router_docs

--- Parcial3/Router/app.js
+++ Parcial3/Router/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors =  require('cors')
+const { query } = require('express');
 const ruta_crud = require('./CRUD')
 
 const app = express();
@@ -9,9 +10,6 @@ app.use(express.json())
 app.use(express.text())
 
 app.use('/get',ruta_crud.router);
-app.use('/geto',ruta_crud.router);
-app.use('/post',ruta_crud.router);
-app.use('/put',ruta_crud.router);
-app.use('/delete',ruta_crud.router);
+
 
 app.listen(8092, ()=>{console.log('Servidor corriendo express')})
