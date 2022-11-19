ecae33ffa72fd6bc35ff14e8c6f037ed6299f7ef
Luis Hdz <luishernandez0626@outlook.com>
2022-11-18 18:54:21 -0600

    Update

--- Parcial3/Router_Doc/Routes/CRUD.js
+++ Parcial3/Router_Doc/Routes/CRUD.js
@@ -18,6 +18,15 @@ con.connect((err) => {
 });
 
 //Get (todos)
+/**
+  * @swagger
+   * /get:
+   *   get:
+   *     description: Muestra a los estudiantes registrados en la BD
+   *     responses:
+   *       200:
+   *         description: Consulta a la BD
+   */
 router.get('/', (req, res) => {
     let sql = 'SELECT * FROM registro';
     let query = con.query(sql,(err,result)=>{
@@ -27,6 +36,21 @@ router.get('/', (req, res) => {
 }); 
 
 //Get (uno)
+/**
+  * @swagger
+   * /geto:
+   *   get:
+   *     description: Muestra los datos de un estudiante en especifico.
+   *     parameters:
+   *       - NumControl: NumControl
+   *         in: query
+   *         description: 'Buscar por medio del Numero de Control al estudiante en especifico.'
+   *         schema:
+   *           type: string
+   *     responses:
+   *       200:
+   *         description: Consulta a la BD
+   */
 router.get('/:NumControl', (req, res) => {
     let sql = `SELECT * FROM registro WHERE NumControl = ${req.params.NumControl}`;
     let query = con.query(sql,(err,result)=>{
@@ -36,6 +60,15 @@ router.get('/:NumControl', (req, res) => {
 }); 
 
 //Post
+/**
+  * @swagger
+   * /post:
+   *   post:
+   *     description: Agregar estudiante a la BD.
+   *     responses:
+   *       200:
+   *         description: Alta a la BD.
+   */
 router.post('/', (req, res) => {
     console.log(req.query);
 
@@ -54,6 +87,21 @@ router.post('/', (req, res) => {
 }); 
 
 //Put
+/**
+  * @swagger
+   * /put:
+   *   put:
+   *     description: Actualiza los datos de un estudiante en especifico por medio del NumControl
+   *     parameters:
+   *       - NumControl: NumControl
+   *         in: query
+   *         description: 'Actualizar datos del estudiante por medio del NumControl'
+   *         schema:
+   *           type: string
+   *     responses:
+   *       200:
+   *         description: Modifica datos a la BD
+   */
 router.put('/:NumControl', (req, res) => {
     console.log(req.query);
 
@@ -73,6 +121,21 @@ router.put('/:NumControl', (req, res) => {
 }); 
 
 //Delete
+/**
+  * @swagger
+   * /delete:
+   *   delete:
+   *     description: Elimina a un estudiante de la BD
+   *     parameters:
+   *       - ID_Jugador: ID_Jugador
+   *         in: query
+   *         description: 'Elimina a un estudiante por medio del NumCotrol'
+   *         schema:
+   *           type: string
+   *     responses:
+   *       200:
+   *         description: Baja a la BD
+   */
 router.delete('/:NumControl', (req, res) => {
     let sql = `DELETE FROM registro WHERE NumControl = ${req.params.NumControl}`;
     let query = con.query(sql,(err,result)=>{
