6ad832f9af365439b9024396a7a46d55721aa21f
HernandezLo <luishernandez0626@outlook.com>
2022-12-02 00:58:10 -0600

    Update

--- API/app.js
+++ API/app.js
@@ -11,7 +11,7 @@ const swaggerJsDoc  = require('swagger-jsdoc');
 
 const swaggerOptions = {definition:{
     openapi: '3.0.0',
-     info: {title: 'API Jugadores Futbol',
+     info: {title: 'API Estudiantes',
      version: '1.0.0',      
     },
     servers:[{url: "http://localhost:8081"}],  
